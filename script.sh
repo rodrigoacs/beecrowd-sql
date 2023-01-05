5638e1f77ae80d119ba09937a493f5231b68cdb7
Rodrigo Soares <rodrigohths@gmail.com>
2023-01-05 15:52:52 -0300

    add: 2737 solution && empty files

--- script.sh
+++ script.sh
@@ -1,1 +1,3 @@
-for i in $(seq 2602 2624); do echo "" >> beecrowd-${i}.sql; done
+# script for create files
+# usage : ./script.sh first last
+for i in $(seq $1 $2); do echo "" >> beecrowd-${i}.sql; done
